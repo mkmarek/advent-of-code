511fb3648c0bb7fcbb96edeac9bda3dec3a6ca3a
Marek Magdziak <marek.magdziak@hotmail.cz>
2019-12-07 09:47:55 +0100

    Smarter approach for Day 7

--- 2019/7.js
+++ 2019/7.js
@@ -3,39 +3,48 @@ const fs = require('fs');
 const input = fs.readFileSync('7.txt').toString().split(',')
     .map(e => e.trim()).map(e => Number(e));
 
-function runProgram(startIndex, inputCursor, instructions, input) {
-    let outputs = [];
-    for (let i = startIndex; i < instructions.length;) {
-        let str = instructions[i].toString().padStart(4, '0');
+function runProgram(instructions, input) {
 
-        const opCode = Number(str.substring(2, 4));
-        const modeParam1 = str.substring(1, 2);
-        const modeParam2 = str.substring(0, 1);
+    let i = 0;
+    let inputCursor = 0;
+    
+    function next(additionalInput) {
+        input = [...input, additionalInput];
+        let output = 0;
+        for (; i < instructions.length;) {
+            let str = instructions[i].toString().padStart(4, '0');
 
-        let a = modeParam1 === '0'
-            ? instructions[instructions[i + 1]]
-            : instructions[i + 1]
+            const opCode = Number(str.substring(2, 4));
+            const modeParam1 = str.substring(1, 2);
+            const modeParam2 = str.substring(0, 1);
 
-        let b = modeParam2 === '0'
-            ? instructions[instructions[i + 2]]
-            : instructions[i + 2]
+            let a = modeParam1 === '0'
+                ? instructions[instructions[i + 1]]
+                : instructions[i + 1]
 
-        switch (opCode) {
-            case 1: instructions[instructions[i + 3]] = a + b; i += 4; break;
-            case 2: instructions[instructions[i + 3]] = a * b; i += 4; break;
-            case 3:
-                if (inputCursor >= input.length) {
-                    return [false, i, inputCursor, input, instructions, outputs]
-                }
-                instructions[instructions[i + 1]] = input[inputCursor++]; i += 2; break;
-            case 4: outputs.push(instructions[instructions[i + 1]]); i += 2; break;
-            case 5: if (a !== 0) i = b; else i += 3; break;
-            case 6: if (a === 0) i = b; else i += 3; break;
-            case 7: if (a < b) instructions[instructions[i + 3]] = 1; else { instructions[instructions[i + 3]] = 0; } i += 4; break;
-            case 8: if (a === b) instructions[instructions[i + 3]] = 1; else instructions[instructions[i + 3]] = 0; i += 4; break;
-            case 99: return [true, i, inputCursor, input, instructions, outputs]
+            let b = modeParam2 === '0'
+                ? instructions[instructions[i + 2]]
+                : instructions[i + 2]
+
+            switch (opCode) {
+                case 1: instructions[instructions[i + 3]] = a + b; i += 4; break;
+                case 2: instructions[instructions[i + 3]] = a * b; i += 4; break;
+                case 3:
+                    if (inputCursor >= input.length) {
+                        return [false, output];
+                    }
+                    instructions[instructions[i + 1]] = input[inputCursor++]; i += 2; break;
+                case 4: output = instructions[instructions[i + 1]]; i += 2; break;
+                case 5: if (a !== 0) i = b; else i += 3; break;
+                case 6: if (a === 0) i = b; else i += 3; break;
+                case 7: if (a < b) instructions[instructions[i + 3]] = 1; else { instructions[instructions[i + 3]] = 0; } i += 4; break;
+                case 8: if (a === b) instructions[instructions[i + 3]] = 1; else instructions[instructions[i + 3]] = 0; i += 4; break;
+                case 99: return [true, output];
+            }
         }
     }
+
+    return next;
 }
 
 // Taken from https://stackoverflow.com/a/20871714
@@ -60,25 +69,20 @@ const permutator = (inputArr) => {
 }
 
 function runForSettings(settings) {
-    let machineStates = [...Array(5)].map(() => [false, 0, 0, [], [...input]]);
-    let machineIndex = 0;
-    let output = 0;
+    let programs = [...Array(5)].map((_, i) => runProgram([...input], [ settings[i] ]));
+    let lastOutput = 0;
+    let lastState = false;
 
-    while (machineStates.filter(e => !e[0]).length > 0) {
-        let [_, instructionIndex, inputCursor, inputs, code] = machineStates[machineIndex];
+    while (!lastState) {
+        for (let i = 0; i < 5; i++) {
+            const [state, result] = programs[i](lastOutput);
 
-        if (instructionIndex === 0 && machineIndex === 0) inputs = [settings[machineIndex], 0]
-        else if (instructionIndex === 0) inputs = [settings[machineIndex], ...inputs]
-
-        machineStates[machineIndex] = runProgram(instructionIndex, inputCursor, code, inputs);
-
-        machineStates[(machineIndex + 1) % 5][3].push(machineStates[machineIndex][5][machineStates[machineIndex][5].length - 1]);
-        machineIndex = (machineIndex + 1) % 5;
+            lastState = state;
+            lastOutput = result;
+        }
     }
 
-    output = machineStates[4][5][0]
-
-    return output;
+    return lastOutput;
 }
 
 function getHighestSignal(settings) {
