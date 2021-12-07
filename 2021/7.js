0f1a8850a322e42766614f630624a2a952108229
Marek Magdziak <marek.magdziak@hotmail.cz>
2021-12-07 19:23:53 +0100

    Smarten up day 7

--- 2021/7.js
+++ 2021/7.js
@@ -1,38 +1,21 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('7.txt').toString().split(',').map(Number);
+let input = fs.readFileSync('7.txt').toString().split(',').filter(e => e).map(Number);
 
 function part1() {
-    let minCost = null;
-    for (let i = 0; i < input.length; i++) {
-        let cost = 0;
-        for (let y = 0; y < input.length; y++) {
-            cost += Math.abs(input[y] - i);
-        }
-
-        if (minCost === null || minCost > cost) {
-            minCost = cost;
-        }
-    }
-
-    return minCost;
+    const arr = input.sort((a, b) => a - b);
+    const median = arr[Math.floor((arr.length + 1) / 2)];
+    return arr.reduce((a, b) => a + Math.abs(b - median), 0);
 }
 
 function part2() {
-    let minCost = null;
-    for (let i = 0; i < input.length; i++) {
-        let cost = 0;
-        for (let y = 0; y < input.length; y++) {
-            const d = Math.abs(input[y] - i);
-            cost += (d / 2) * (d + 1)
-        }
+    const mean = Math.ceil(input.reduce((a, b) => a + b, 0) / input.length);
 
-        if (minCost === null || minCost > cost) {
-            minCost = cost;
-        }
-    }
-
-    return minCost;
+    const score = (input) => input.reduce((a, b) => a + (b / 2) * (b + 1), 0)
+    return Math.min(
+        score(input.map(e => Math.abs(e - mean))),
+        score(input.map(e => Math.abs(e - mean + 1)))
+    );
 }
 
 console.log(`Part1: ${part1()}`)
---
+++ 2021/8.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('8.txt').toString().split(',');
+
+function part1() {
+    
+}
+
+function part2() {
+    
+}
+
+console.log(`Part1: ${part1()}`)
+console.log(`Part2: ${part2()}`)
