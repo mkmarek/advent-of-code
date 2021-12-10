decc936179133210194d3827af0b2d821fe58f5b
Marek Magdziak <marek.magdziak@hotmail.cz>
2021-12-10 07:07:47 +0100

    Some more day 10 cleanup

--- 2021/10.js
+++ 2021/10.js
@@ -1,67 +1,44 @@
 const fs = require('fs');
-const { sum, groupBy, toKeyValue, distinct, intersect, getNeighbors, getNeighborsDiagonal, iterateTwoDimArray } = require('../utils');
-
 const input = fs.readFileSync('10.txt').toString().split('\n').map(e => e.trim());
 
-function isOpen(char) {
-    return char === '(' || char === '[' || char === '{' || char === '<'
-}
-
-function getOpposite(char) {
-    if (char === '(') {
-        return ')'
-    } else if (char === '[') {
-        return ']'
-    } else if (char === '{') {
-        return '}'
-    } else if (char === '<') {
-        return '>'
-    }
-}
+const open = ['(', '[', '{', '<'];
+const opposite = { '(': ')', '[': ']', '{': '}', '<': '>' }
 
 function part1() {
-    const invalid = [];
+    let score = 0;
     for (let i = 0; i < input.length; i++) {
-        let expectedStack = []
+        let stack = []
         for (let y = 0; y < input[i].length; y++) {
-            if (isOpen(input[i][y])) {
-                expectedStack.push(getOpposite(input[i][y]))
-            } else {
-                if (expectedStack.pop() !== input[i][y]) {
-                    invalid.push(input[i][y])
-                    break;
-                }
+            if (open.includes(input[i][y])) {
+                stack.push(opposite[input[i][y]])
+            } else if (stack.pop() !== input[i][y]) {
+                score += { ')': 3, ']': 57, '}': 1197, '>': 25137 }[input[i][y]];
+                break;
             }
         }
     }
 
-    return sum(invalid, c => ({ ')': 3, ']': 57, '}': 1197, '>': 25137 }[c]));
+    return score;
 }
 
 function part2() {
     const scores = [];
 
     for (let i = 0; i < input.length; i++) {
-        const expectedStack = []
-        let isvalid = true;
+        const stack = []
         for (let y = 0; y < input[i].length; y++) {
-            if (isOpen(input[i][y])) {
-                expectedStack.push(getOpposite(input[i][y]))
-            } else if (expectedStack.length > 0) {
-                if (expectedStack.pop() !== input[i][y]) {
-                    isvalid = false;
-                    break;
-                }
+            if (open.includes(input[i][y])) {
+                stack.push(opposite[input[i][y]])
+            } else if (stack.length > 0 && stack.pop() !== input[i][y]) {
+                break;
             }
-        }
-
-        if (isvalid) {
-            expectedStack.reverse()
-            scores.push(expectedStack.reduce((acc, cur) => acc * 5 + ({ ')': 1, ']': 2, '}': 3, '>': 4 }[cur]), 0));
+            if (y === input[i].length - 1) {
+                scores.push(stack.reverse().reduce((acc, cur) => acc * 5 + ({ ')': 1, ']': 2, '}': 3, '>': 4 }[cur]), 0));
+            }
         }
     }
 
-    return scores.sort((a, b) => a - b)[Math.floor(scores.length /2)];
+    return scores.sort((a, b) => a - b)[Math.floor(scores.length / 2)];
 }
 
 console.log(`Part1: ${part1()}`)
