ec8522eef189b8112247f18c8408843c3811245d
Marek Magdziak <marek.magdziak@hotmail.cz>
2021-12-11 06:27:18 +0100

    Cleanup day 11

--- 2021/11.js
+++ 2021/11.js
@@ -1,80 +1,59 @@
 const fs = require('fs');
-const { sum, groupBy, toKeyValue, distinct, intersect, getNeighbors, getNeighborsDiagonal, iterateTwoDimArray } = require('../utils');
+const { getNeighborsDiagonal, iterateTwoDimArray } = require('../utils');
 
 const data = fs.readFileSync('11.txt').toString().split('\n').map(e => e.trim().split('').map(x => parseInt(x.trim())));
 
-function part1() {
-    const input = [...data.map(e => ([...e]))];
+function simulate(input, onFlashed) {
+    let toIncrease = [];
+    const flashed = {};
+
+    iterateTwoDimArray(input, (_, x, y) => {
+        toIncrease.push([x, y])
+    });
+
+    while (toIncrease.length > 0) {
+        const p = toIncrease.pop();
+
+        if (flashed[`${p[0]}_${p[1]}`]) continue;
+
+        input[p[0]][p[1]]++;
+
+        if (input[p[0]][p[1]] > 9) {
+            onFlashed(p);
+
+            input[p[0]][p[1]] = 0;
+            flashed[`${p[0]}_${p[1]}`] = true;
+
+            toIncrease.push(...getNeighborsDiagonal(p, input).filter(n => !flashed[`${n[0]}_${n[1]}`]));
+        }
+    }
+}
+
+function part1(input) {
     let flashes = 0;
 
     for (let i = 0; i < 100; i++) {
-        let flashed = {};
-        let toIncrease = [];
-        iterateTwoDimArray(input, (value, x, y) => {
-            toIncrease.push([x, y])
-        });
-
-        while (toIncrease.length > 0) {
-            let p = toIncrease.pop();
-            input[p[0]][p[1]]++;
-
-            if (input[p[0]][p[1]] > 9) {
-                flashes++;
-                input[p[0]][p[1]] = 0;
-                flashed[`${p[0]}_${p[1]}`] = true;
-                toIncrease = toIncrease.filter(e => !flashed[`${e[0]}_${e[1]}`]);
-
-                let neighborsDiagonal = getNeighborsDiagonal(p, input);
-                for (let i = 0; i < neighborsDiagonal.length; i++) {
-                    let n = neighborsDiagonal[i];
-                    if (!flashed[`${n[0]}_${n[1]}`]) {
-                        toIncrease.push(n);
-                    }
-                }
-            }
-        }
+        simulate(input, () => { flashes++ });        
     }
 
     return flashes;
 }
 
-function part2() {
-    const input = [...data.map(e => ([...e]))];
+function part2(input) {
     const totalOcto = input.reduce((acc, curr) => acc + curr.length, 0);
 
     let steps = 0;
     while (true) {
-        let flashed = {};
-        let toIncrease = [];
-        iterateTwoDimArray(input, (value, x, y) => {
-            toIncrease.push([x, y])
-        });
-
-        while (toIncrease.length > 0) {
-            let p = toIncrease.pop();
-            input[p[0]][p[1]]++;
-
-            if (input[p[0]][p[1]] > 9) {
-                input[p[0]][p[1]] = 0;
-                flashed[`${p[0]}_${p[1]}`] = true;
-                toIncrease = toIncrease.filter(e => !flashed[`${e[0]}_${e[1]}`]);
-
-                let neighborsDiagonal = getNeighborsDiagonal(p, input);
-                for (let i = 0; i < neighborsDiagonal.length; i++) {
-                    let n = neighborsDiagonal[i];
-                    if (!flashed[`${n[0]}_${n[1]}`]) {
-                        toIncrease.push(n);
-                    }
-                }
-            }
+        let flashes = 0;
+        simulate(input, () => { flashes++ });   
+        
+        if (flashes === totalOcto) {
+            return steps + 1;
         }
-
-        if (Object.keys(flashed).length === totalOcto) {
-            return steps;
-        }
+        
         steps++;
     }
 }
 
-console.log(`Part1: ${part1()}`)
-console.log(`Part2: ${part2() + 1}`)
+console.log(`Part1: ${part1([...data.map(e => ([...e]))])}`)
+console.log(`Part2: ${part2([...data.map(e => ([...e]))])}`)
