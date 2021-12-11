f2db4482d7f27aa69d2687d80d15c57875dea636
Marek Magdziak <marek.magdziak@hotmail.cz>
2021-12-11 06:40:38 +0100

    Some last cleanup of day 11 and preparation for day 12

--- 2021/11.js
+++ 2021/11.js
@@ -3,56 +3,42 @@ const { getNeighborsDiagonal, iterateTwoDimArray, deepCopy } = require('../utils
 
 const data = fs.readFileSync('11.txt').toString().split('\n').map(e => e.trim().split('').map(x => parseInt(x.trim())));
 
-function simulate(input, onFlashed) {
-    let toIncrease = [];
+function step(input) {
+    const positions = [];
     const flashed = {};
 
     iterateTwoDimArray(input, (_, x, y) => {
-        toIncrease.push([x, y])
+        positions.push([x, y])
     });
 
-    while (toIncrease.length > 0) {
-        const p = toIncrease.pop();
+    while (positions.length > 0) {
+        const p = positions.pop();
 
         if (flashed[`${p[0]}_${p[1]}`]) continue;
 
         input[p[0]][p[1]]++;
 
         if (input[p[0]][p[1]] > 9) {
-            onFlashed(p);
-
             input[p[0]][p[1]] = 0;
             flashed[`${p[0]}_${p[1]}`] = true;
 
-            toIncrease.push(...getNeighborsDiagonal(p, input).filter(n => !flashed[`${n[0]}_${n[1]}`]));
+            positions.push(...getNeighborsDiagonal(p, input));
         }
     }
+
+    return Object.keys(flashed).length;
 }
 
 function part1(input) {
-    let flashes = 0;
-
-    for (let i = 0; i < 100; i++) {
-        simulate(input, () => { flashes++ });        
-    }
-
-    return flashes;
+    return new Array(100).fill(0).reduce((acc) => acc + step(input), 0);
 }
 
 function part2(input) {
     const totalOcto = input.reduce((acc, curr) => acc + curr.length, 0);
 
-    let steps = 0;
-    while (true) {
-        let flashes = 0;
-        simulate(input, () => { flashes++ });   
-        
-        if (flashes === totalOcto) {
-            return steps + 1;
-        }
-        
-        steps++;
-    }
+    let steps = 1;
+    while (step(input) !== totalOcto) steps++;
+    return steps;
 }
 
 console.log(`Part1: ${part1(deepCopy(data))}`)
---
+++ 2021/12.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const { sum, getNeighbors, iterateTwoDimArray, deepCopy } = require('../utils');
+
+const data = fs.readFileSync('12.txt').toString().split('\n');
+
+function part1(input) {
+    
+}
+
+function part2(input) {
+    
+}
+
+console.log(`Part1: ${part1(deepCopy(data))}`)
+console.log(`Part2: ${part2(deepCopy(data))}`)
