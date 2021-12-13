d9166a630930f5f4b37e71552ae0834ecca47112
Marek Magdziak <marek.magdziak@hotmail.cz>
2021-12-13 06:46:46 +0100

    Day 13 cleanup

--- 2021/13.js
+++ 2021/13.js
@@ -1,97 +1,71 @@
 const fs = require('fs');
-const { deepCopy, toKeyValue, distinct, groupBy, intersect, iterateTwoDimArray } = require('../utils');
+const { deepCopy, distinct } = require('../utils');
 
 const data = fs.readFileSync('13.txt').toString().split('\n').map(line => line.trim());
 
-function fold(points, width, height, axis, value) {
-    const pointsCopy = [];
-    if (axis === 'x') {
-        for (let x = 0; x <= value; x++) {
-            for (let y = 0; y < height; y++) {
-                if (points.find(point => point[0] === x && point[1] === y) || points.find(point => point[0] === (width - x - 1) && point[1] === y)) {
-                    pointsCopy.push([x, y]);
-                }
-            }
-        }
-    } else {
-        for (let x = 0; x < width; x++) {
-            for (let y = 0; y <= value; y++) {
-                if (points.find(point => point[0] === x && point[1] === y) || points.find(point => point[0] === x && point[1] === (height - y - 1))) {
-                    pointsCopy.push([x, y]);
-                }
-            }
-        }
+function fold(points, axis, value) {
+    const [width, height] = getSize(points);
+
+    return axis == 'x'
+        ? distinct(points.map(p => p[0] > value ? [width - p[0] - 1, p[1]] : p))
+        : distinct(points.map(p => p[1] > value ? [p[0], height - p[1] - 1] : p))
+}
+
+function getSize(points) {
+    let maxX = 0;
+    let maxY = 0;
+    for (let i = 0; i < points.length; i++) {
+        if (maxX < points[i][0]) maxX = points[i][0];
+        if (maxY < points[i][1]) maxY = points[i][1];
     }
 
-    return pointsCopy;
+    return [maxX + 1, maxY + 1];
+}
+
+function getPointsAndFolds(input) {
+    const points = [];
+    const folds = [];
+    let i;
+
+    for (i = 0; i < input.length; i++) {
+        if (input[i] === '') break;
+        points.push(input[i].split(',').map(Number));
+    }
+
+    i++;
+
+    for (; i < input.length; i++) {
+        folds.push({ axis: input[i].split('=')[0].split(' ')[2], value: Number(input[i].split('=')[1]) });
+    }
+
+    return [points, folds];
+}
+
+function printPoints(points) {
+    const [width, height] = getSize(points);
+    let result = '';
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            result += points.find(dot => dot[0] === x && dot[1] === y) ? '#' : ' ';
+        }
+        result += '\n';
+    }
+    return result;
 }
 
 function part1(input) {
-    const dots = [];
-    const folds = [];
-    let i;
-    for (i = 0; i < input.length; i++) {
-        if (input[i] === '') break;
-        dots.push(input[i].split(',').map(Number));
-    }
-    i++;
-
-    for (; i < input.length; i++) {
-        folds.push({ axis: input[i].split('=')[0].split(' ')[2], value: Number(input[i].split('=')[1]) });
-    }
-
-    let maxX = 0;
-    let maxY = 0;
-    for (let i = 0; i < dots.length; i++) {
-        if (maxX < dots[i][0]) maxX = dots[i][0];
-        if (maxY < dots[i][1]) maxY = dots[i][1];
-    }
-
-    const newPoitns = fold(dots, maxX + 1, maxY + 1, folds[0].axis, folds[0].value);
-    return newPoitns.length;
+    const [points, folds] = getPointsAndFolds(input);
+    return fold(points, folds[0].axis, folds[0].value).length;
 }
 
 function part2(input) {
-    let dots = [];
-    const folds = [];
-    let i;
-    for (i = 0; i < input.length; i++) {
-        if (input[i] === '') break;
-        dots.push(input[i].split(',').map(Number));
-    }
-    i++;
-
-    for (; i < input.length; i++) {
-        folds.push({ axis: input[i].split('=')[0].split(' ')[2], value: Number(input[i].split('=')[1]) });
-    }
-
-    let maxX = 0;
-    let maxY = 0;
-    for (let i = 0; i < dots.length; i++) {
-        if (maxX < dots[i][0]) maxX = dots[i][0];
-        if (maxY < dots[i][1]) maxY = dots[i][1];
-    }
+    let [points, folds] = getPointsAndFolds(input);
 
     for (let f of folds) {
-        dots = fold(dots, maxX + 1, maxY + 1, f.axis, f.value);
-
-        maxX = 0;
-        maxY = 0;
-        for (let i = 0; i < dots.length; i++) {
-            if (maxX < dots[i][0]) maxX = dots[i][0];
-            if (maxY < dots[i][1]) maxY = dots[i][1];
-        }
+        points = fold(points, f.axis, f.value);
     }
 
-    let result = '\n';
-    for (let y = 0; y <= maxY; y++) {
-        let str = ''
-        for (let x = 0; x <= maxX; x++) {
-            str += dots.find(dot => dot[0] === x && dot[1] === y) ? '#' : '.';
-        }
-        result += str + '\n';
-    }
-    return result;
+    return `\n${printPoints(points)}`;
 }
 
 console.log(`Part1: ${part1(deepCopy(data))}`)
