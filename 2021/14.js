6dee1dec24fa9e0b353b17294871c9794b0acc60
Marek Magdziak <marek.magdziak@hotmail.cz>
2021-12-14 07:49:16 +0100

    Cleanup

--- 2021/14.js
+++ 2021/14.js
@@ -1,7 +1,4 @@
 const fs = require("fs");
-const {
-  deepCopy,
-} = require("../utils");
 
 const data = fs
   .readFileSync("14.txt")
@@ -15,69 +12,23 @@ const rules = data
   .filter((e) => e)
   .map((e) => e.split(" -> "));
 
-function part1(template, rules) {
-  for (let step = 0; step < 10; step++) {
-    for (let i = 0; i < template.length - 1; i++) {
-      const rule = rules.find(
-        (e) => e[0][0] === template[i] && e[0][1] === template[i + 1]
-      );
-      if (rule) {
-        template.splice(i + 1, 0, rule[1]);
-        i++;
-      }
-    }
-  }
+function run(template, rules, steps) {
+  let pairs = template.slice(1).reduce((acc, e, i) => ({ ...acc, [`${template[i]}${e}`]: (acc[`${template[i]}${e}`] || 0) + 1 }), {});
+  const occurences = template.reduce((acc, e, i) => ({ ...acc, [e]: (acc[e] || 0) + 1 }), {});
 
-  const occurences = {};
-  for (let i = 0; i < template.length; i++) {
-    if (occurences[template[i]]) {
-      occurences[template[i]]++;
-    } else {
-      occurences[template[i]] = 1;
-    }
-  }
-
-  return (
-    Math.max(...Object.values(occurences)) -
-    Math.min(...Object.values(occurences))
-  );
-}
-
-function part2(template, rules) {
-  let pairs = {};
-  for (let i = 0; i < template.length - 1; i++) {
-    if (!pairs[template[i] + template[i + 1]])
-      pairs[template[i] + template[i + 1]] = 0;
-    pairs[template[i] + template[i + 1]]++;
-  }
-
-  const occurences = {};
-  for (let i = 0; i < template.length; i++) {
-    if (!occurences[template[i]]) occurences[template[i]] = 0;
-    occurences[template[i]]++;
-  }
-
-  for (let step = 0; step < 40; step++) {
-    const keys = Object.keys(pairs);
+  for (let step = 0; step < steps; step++) {
     const newPairs = { ...pairs };
-    for (let i = 0; i < keys.length; i++) {
-      if (!pairs[keys[i]]) continue;
-      const rule = rules.find((e) => e[0] === keys[i]);
+    for (const key of Object.keys(pairs)) {
+      if (!pairs[key]) continue;     
+      const rule = rules.find((e) => e[0] === key);
       if (rule) {
-        const onePair = `${keys[i][0]}${rule[1]}`;
-        const secondPair = `${rule[1]}${keys[i][1]}`;
+        const first = `${key[0]}${rule[1]}`;
+        const second = `${rule[1]}${key[1]}`;
 
-        if (!newPairs[onePair]) newPairs[onePair] = 0;
-        if (!newPairs[secondPair]) newPairs[secondPair] = 0;
-
-        const val = pairs[keys[i]];
-
-        newPairs[keys[i]] -= val;
-        newPairs[onePair] += val;
-        newPairs[secondPair] += val;
-
-        if (!occurences[rule[1]]) occurences[rule[1]] = 0;
-        occurences[rule[1]] += val;
+        newPairs[key] -= pairs[key];
+        newPairs[first] = (newPairs[first] || 0) + pairs[key];
+        newPairs[second] = (newPairs[second] || 0) + pairs[key];
+        occurences[rule[1]] = (occurences[rule[1]] || 0) + pairs[key];
       }
     }
     pairs = newPairs;
@@ -89,5 +40,13 @@ function part2(template, rules) {
   );
 }
 
-console.log(`Part1: ${part1(deepCopy(template), rules)}`);
-console.log(`Part2: ${part2(deepCopy(template), rules)}`);
+function part1(template, rules) {
+  return run(template, rules, 10);
+}
+
+function part2(template, rules) {
+  return run(template, rules, 40);
+}
+
+console.log(`Part1: ${part1(template, rules)}`);
+console.log(`Part2: ${part2(template, rules)}`);
