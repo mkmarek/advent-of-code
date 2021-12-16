0b32e866e287fbb196bcab9b38771a26338cc119
Marek Magdziak <marek.magdziak@hotmail.cz>
2021-12-16 08:35:21 +0100

    Day 16 cleanup

--- 2021/16.js
+++ 2021/16.js
@@ -5,150 +5,116 @@ const data = fs.readFileSync('16.txt').toString().trim()
   .split('')
   .map(line => line.trim());
 
-const hexToBin = `0 = 0000
-1 = 0001
-2 = 0010
-3 = 0011
-4 = 0100
-5 = 0101
-6 = 0110
-7 = 0111
-8 = 1000
-9 = 1001
-A = 1010
-B = 1011
-C = 1100
-D = 1101
-E = 1110
-F = 1111`.split('\n').map(line => line.trim().split(' = '));
+function hex2bin(hex){
+  return parseInt(hex, 16).toString(2).padStart(4, '0');
+}
 
-function part1(input) {
-  const binary = input.map(hex => hexToBin.find(line => line[0] === hex)[1]).join('');
-  // const binary = '110100101111111000101000'
-  const packets = [];
-  for (let i = 0; i < binary.length;) {
-    const version = parseInt(binary.slice(i, i + 3), 2);
-    i+=3;
-    const typeId = parseInt(binary.slice(i, i + 3), 2);
-    i+=3;
-    let literal = '';
-    let subpacketsInBits = null;
-    let numberOfSubpackets = null;
-    if (typeId == 4) {
-      let bits = '';
-      while (true) {
-        const fiveBits = binary.slice(i, i + 5);
-        bits += fiveBits.slice(1);;
-        i+=5;
+function sumVersions(packet) {
+  return packet.version + packet.children.reduce((acc, ver) => acc + sumVersions(ver), 0);
+}
 
-        if (fiveBits[0] === '0') break;
-      }
+function parseLiteral(index, binary) {
+  let bits = '';
+  while (true) {
+    const fiveBits = binary.slice(index, index + 5);
+    bits += fiveBits.slice(1);
+    index += 5;
 
-      literal = `${parseInt(bits, 2)}`
-    } else {
-      const lengthTypeId = binary[i];
-      i++;
-      if ( lengthTypeId == '0') {
-        subpacketsInBits = parseInt(binary.slice(i, i + 15), 2);
-        i+=15;
-      }
-      if (lengthTypeId == '1') {
-        const length = parseInt(binary.slice(i, i + 11), 2);
-        i+=11;
-        numberOfSubpackets = length;
-      }
-    }
-
-    packets.push({ version, typeId, literal, subpacketsInBits, numberOfSubpackets });
+    if (fiveBits[0] === '0') break;
   }
-  return packets.reduce((acc, ver) => acc + ver.version, 0);
+
+  return [parseInt(bits, 2), index];
+}
+
+function parseChildrenWithLength(index, binary) {
+  const length = parseInt(binary.slice(index, index + 11), 2);
+  index += 11;
+
+  const children = [];
+
+  for (let s = 0; s < length; s++) {
+    const packet = parse(binary.slice(index));
+    index += packet.len;
+    children.push(packet);
+  }
+
+  return [children, index];
+}
+
+function parseChildrenWithByteLength(index, binary) {
+  let childrenInBits = parseInt(binary.slice(index, index + 15), 2);
+  index += 15;
+
+  let len = 0;
+  const children = [];
+
+  while (len < childrenInBits) {
+    const pack = parse(binary.slice(index, index + childrenInBits));
+    len += pack.len;
+    index += pack.len;
+    children.push(pack);
+  }
+  
+  return [children, index];
 }
 
 function parse(binary) {
-  let i = 0;
-  const at = i;
-  const version = parseInt(binary.slice(i, i + 3), 2);
-  i+=3;
-  const typeId = parseInt(binary.slice(i, i + 3), 2);
-  i+=3;
-  let literal = '';
-  let subpackets = [];
-  if (typeId == 4) {
-    let bits = '';
-    while (true) {
-      const fiveBits = binary.slice(i, i + 5);
-      bits += fiveBits.slice(1);;
-      i+=5;
+  let cursor = 0;
+  const packet = {};
 
-      if (fiveBits[0] === '0') break;
-    }
+  packet.version = parseInt(binary.slice(cursor, cursor + 3), 2);
+  cursor+=3;
 
-    literal = `${parseInt(bits, 2)}`
+  packet.typeId = parseInt(binary.slice(cursor, cursor + 3), 2);
+  cursor+=3;
+
+  if (packet.typeId == 4) {
+    const [lit, nextIndex] = parseLiteral(cursor, binary);
+
+    packet.literal = lit;
+    packet.children = [];
+    cursor = nextIndex;
   } else {
-    const lengthTypeId = binary[i];
-    i++;
-    if ( lengthTypeId == '0') {
-      let subpacketsInBits = parseInt(binary.slice(i, i + 15), 2);
-      let len = 0;
+    const lengthTypeId = binary[cursor];
+    cursor++;
 
-      while (len < subpacketsInBits) {
-        const pack = parse(binary.slice(i + 15, i + 15 + subpacketsInBits));
-        len += pack.len;
-        i+= pack.len;
-        subpackets.push(pack);
-      }
-      
-      i+=15;
-    }
-    if (lengthTypeId == '1') {
-      const length = parseInt(binary.slice(i, i + 11), 2);
-      i+=11;
-      for (let s = 0; s < length; s++) {
-        const packet = parse(binary.slice(i));
-        i+=packet.len;
-        subpackets.push(packet);
-      }
+    if (lengthTypeId == '0') {
+      const [children, nextIndex] = parseChildrenWithByteLength(cursor, binary);
+      packet.children = children;
+      cursor = nextIndex;
+    } else {
+      const [children, nextIndex] = parseChildrenWithLength(cursor, binary);
+      packet.children = children;
+      cursor = nextIndex;
     }
   }
 
-  return { len: i - at, version, typeId, literal, subpackets };
+  packet.len = cursor;
+
+  return packet;
 }
 
-function part2(input) {
-  const binary = input.map(hex => hexToBin.find(line => line[0] === hex)[1]).join('');
-  
-  const packets = parse(binary);
-  
-  return evalp(packets);
-}
-
-function evalp(packet) {
-  // console.log(index, packets[index].typeId);
+function evalPacket(packet) {
   switch (packet.typeId) {
-    case 0: {
-      // console.log('++++', subpackets);
-      return packet.subpackets.reduce((acc, ver) => acc + evalp(ver), 0)
-    }
-    case 4: { const lit = Number(packet.literal);  return lit; }
-    case 1: {
-      // console.log('*********', subpackets);
-      return packet.subpackets.reduce((acc, ver) => acc * evalp(ver), 1);
-    }
-    case 2: return packet.subpackets.reduce((acc, ver) => Math.min(acc, evalp(ver)), 999999999999999999);
-    case 3: return packet.subpackets.reduce((acc, ver) => Math.max(acc, evalp(ver,)), 0);
-    case 5: { 
-      return evalp(packet.subpackets[0]) > evalp(packet.subpackets[1]) ? 1 : 0;
-    }
-    case 6: { 
-      return evalp(packet.subpackets[0]) < evalp(packet.subpackets[1]) ? 1 : 0;
-    }
-    case 7: { 
-      // console.log('xxx', a, b);
-      return evalp(packet.subpackets[0]) === evalp(packet.subpackets[1]) ? 1 : 0;
-    }
+    case 0: return packet.children.reduce((acc, ver) => acc + evalPacket(ver), 0);
+    case 1: return packet.children.reduce((acc, ver) => acc * evalPacket(ver), 1);
+    case 2: return packet.children.reduce((acc, ver) => Math.min(acc, evalPacket(ver)), 999999999999999999);
+    case 3: return packet.children.reduce((acc, ver) => Math.max(acc, evalPacket(ver,)), 0);
+    case 4: return Number(packet.literal);
+    case 5: return evalPacket(packet.children[0]) > evalPacket(packet.children[1]) ? 1 : 0;
+    case 6: return evalPacket(packet.children[0]) < evalPacket(packet.children[1]) ? 1 : 0;
+    case 7: return evalPacket(packet.children[0]) === evalPacket(packet.children[1]) ? 1 : 0;
   }
 
   throw new Error('Unknown packet type: ' + packets[index].typeId);
+}
+
+function part1(input) {
+  return sumVersions(parse(input.map(hex => hex2bin(hex)).join('')));
+}
+
+function part2(input) {
+  return evalPacket(parse(input.map(hex => hex2bin(hex)).join('')));
 }
 
 console.log(`Part1: ${part1(deepCopy(data))}`)
