6517b142994dbdfc3ca2d1dd65a12ae60c2a2ccc
Marek Magdziak <marek.magdziak@hotmail.cz>
2022-12-06 06:28:49 +0100

    More cleanup

--- 2022/6.js
+++ 2022/6.js
@@ -1,24 +1,17 @@
-const fs = require('fs');
-const { sum, groupBy, toKeyValue, intersect, distinct, iterateTwoDimArray} = require('../utils');
+const fs = require("fs");
 
-const input = fs.readFileSync('6.txt').toString()
-  .trim()
+const input = fs.readFileSync("6.txt").toString().trim();
 
 function scan(uniqueCharacterSize) {
   for (let i = uniqueCharacterSize; i < input.length; i++) {
-    if (new Set(input.substring(i - uniqueCharacterSize, i)).size === uniqueCharacterSize) {
-      return i
+    if (
+      new Set(input.substring(i - uniqueCharacterSize, i)).size ===
+      uniqueCharacterSize
+    ) {
+      return i;
     }
   }
 }
 
-function part1() {
-  return scan(4);
-}
-
-function part2() {
-  return scan(14);
-}
-
-console.log(`Part1: ${part1()}`)
-console.log(`Part2: ${part2()}`)
+console.log(`Part1: ${scan(4)}`);
+console.log(`Part2: ${scan(14)}`);
