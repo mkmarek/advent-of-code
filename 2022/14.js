b29c2784c4fd989456145c1209e6f12a9e63c707
Marek Magdziak <marek.magdziak@hotmail.cz>
2022-12-14 07:01:12 +0100

    Cleanup

--- 2022/14.js
+++ 2022/14.js
@@ -24,73 +24,78 @@ const input = fs
       .map((e) => e.split(",").map(Number))
   );
 
-function part1() {
-  let bounds = { x: 500, y: 0, w: 500, h: 0 };
-  let grid = [];
+function getMap() {
+  const map = {};
+  const bounds = { x: 500, y: 0, w: 500, h: 0 };
 
   for (let i = 0; i < input.length; i++) {
-    let lines = input[i];
+    const lines = input[i];
+
     for (let j = 0; j < lines.length - 1; j++) {
-      let from = lines[j];
-      let to = lines[j + 1];
+      const from = lines[j];
+      const to = lines[j + 1];
 
       bounds.x = Math.min(bounds.x, from[0], to[0]);
       bounds.w = Math.max(bounds.w, from[0], to[0]);
       bounds.y = Math.min(bounds.y, from[1], to[1]);
       bounds.h = Math.max(bounds.h, from[1], to[1]);
 
-      if (from[0] !== to[0]) {
-        for (let x = from[0]; x !== to[0]; x += Math.sign(to[0] - from[0])) {
-          grid[x] = grid[x] || [];
-          grid[x][from[1]] = "#";
-        }
-
-        grid[to[0]] = grid[to[0]] || [];
-        grid[to[0]][from[1]] = "#";
-      }
-
       if (from[1] !== to[1]) {
         for (let y = from[1]; y !== to[1]; y += Math.sign(to[1] - from[1])) {
-          grid[from[0]] = grid[from[0]] || [];
-          grid[from[0]][y] = "#";
+          map[from[0] + "," + y] = true;
         }
+        map[from[0] + "," + to[1]] = true;
+      }
 
-        grid[from[0]] = grid[from[0]] || [];
-        grid[from[0]][to[1]] = "#";
-
+      if (from[0] !== to[0]) {
+        for (let x = from[0]; x !== to[0]; x += Math.sign(to[0] - from[0])) {
+          map[x + "," + from[1]] = true;
+        }
+        map[to[0] + "," + from[1]] = true;
       }
     }
   }
 
-  let grains = {};
+  return [map, bounds];
+}
+
+function moveGrain(map, grains, grain) {
+  const gridBelowEmpty = !map[grain[0] + "," + (grain[1] + 1)];
+  const grainBelowEmpty = !grains[grain[0] + "," + (grain[1] + 1)];
+  const gridLeftBelowEmpty = !map[grain[0] - 1 + "," + (grain[1] + 1)];
+  const grainLeftBelowEmpty = !grains[grain[0] - 1 + "," + (grain[1] + 1)];
+  const gridRightBelowEmpty = !map[grain[0] + 1 + "," + (grain[1] + 1)];
+  const grainRightBelowEmpty = !grains[grain[0] + 1 + "," + (grain[1] + 1)];
+
+  if (gridBelowEmpty && grainBelowEmpty) {
+    grain[1]++;
+  } else if (gridLeftBelowEmpty && grainLeftBelowEmpty) {
+    grain[0]--;
+    grain[1]++;
+  } else if (gridRightBelowEmpty && grainRightBelowEmpty) {
+    grain[0]++;
+    grain[1]++;
+  } else {
+    return false;
+  }
+
+  return true;
+}
+
+function part1() {
+  const [map, bounds] = getMap();
+
+  const grains = {};
   while (true) {
-    let grain = [500, 0];
+    const grain = [500, 0];
 
     while (true) {
-      const gridBelowEmpty =
-        ((grid[grain[0]] || [])[grain[1] + 1] || ".") === ".";
-      const grainBelowEmpty = !grains[grain[0] + "," + (grain[1] + 1)];
-      const gridLeftBelowEmpty =
-        ((grid[grain[0] - 1] || [])[grain[1] + 1] || ".") === ".";
-      const grainLeftBelowEmpty = !grains[grain[0] - 1 + "," + (grain[1] + 1)];
-      const gridRightBelowEmpty =
-        ((grid[grain[0] + 1] || [])[grain[1] + 1] || ".") === ".";
-      const grainRightBelowEmpty = !grains[grain[0] + 1 + "," + (grain[1] + 1)];
-
-      if (gridBelowEmpty && grainBelowEmpty) {
-        grain[1]++;
-      } else if (gridLeftBelowEmpty && grainLeftBelowEmpty) {
-        grain[0]--;
-        grain[1]++;
-      } else if (gridRightBelowEmpty && grainRightBelowEmpty) {
-        grain[0]++;
-        grain[1]++;
-      } else {
+      if (!moveGrain(map, grains, grain)) {
         break;
       }
 
       if (grain[1] > bounds.h) {
-        return Object.keys(grains).length
+        return Object.keys(grains).length;
       }
     }
 
@@ -99,67 +104,14 @@ function part1() {
 }
 
 function part2() {
-  let bounds = { x: 500, y: 0, w: 500, h: 0 };
-  let grid = [];
-
-  for (let i = 0; i < input.length; i++) {
-    let lines = input[i];
-    for (let j = 0; j < lines.length - 1; j++) {
-      let from = lines[j];
-      let to = lines[j + 1];
-
-      bounds.x = Math.min(bounds.x, from[0], to[0]);
-      bounds.w = Math.max(bounds.w, from[0], to[0]);
-      bounds.y = Math.min(bounds.y, from[1], to[1]);
-      bounds.h = Math.max(bounds.h, from[1], to[1]);
-
-      if (from[0] !== to[0]) {
-        for (let x = from[0]; x !== to[0]; x += Math.sign(to[0] - from[0])) {
-          grid[x] = grid[x] || [];
-          grid[x][from[1]] = "#";
-        }
-
-        grid[to[0]] = grid[to[0]] || [];
-        grid[to[0]][from[1]] = "#";
-      }
-
-      if (from[1] !== to[1]) {
-        for (let y = from[1]; y !== to[1]; y += Math.sign(to[1] - from[1])) {
-          grid[from[0]] = grid[from[0]] || [];
-          grid[from[0]][y] = "#";
-        }
-
-        grid[from[0]] = grid[from[0]] || [];
-        grid[from[0]][to[1]] = "#";
-
-      }
-    }
-  }
+  const [map, bounds] = getMap();
 
   let grains = {};
   while (true) {
     let grain = [500, 0];
 
     while (true) {
-      const gridBelowEmpty =
-        ((grid[grain[0]] || [])[grain[1] + 1] || ".") === ".";
-      const grainBelowEmpty = !grains[grain[0] + "," + (grain[1] + 1)];
-      const gridLeftBelowEmpty =
-        ((grid[grain[0] - 1] || [])[grain[1] + 1] || ".") === ".";
-      const grainLeftBelowEmpty = !grains[grain[0] - 1 + "," + (grain[1] + 1)];
-      const gridRightBelowEmpty =
-        ((grid[grain[0] + 1] || [])[grain[1] + 1] || ".") === ".";
-      const grainRightBelowEmpty = !grains[grain[0] + 1 + "," + (grain[1] + 1)];
-
-      if (gridBelowEmpty && grainBelowEmpty) {
-        grain[1]++;
-      } else if (gridLeftBelowEmpty && grainLeftBelowEmpty) {
-        grain[0]--;
-        grain[1]++;
-      } else if (gridRightBelowEmpty && grainRightBelowEmpty) {
-        grain[0]++;
-        grain[1]++;
-      } else {
+      if (!moveGrain(map, grains, grain)) {
         break;
       }
 
@@ -168,10 +120,10 @@ function part2() {
       }
     }
 
-    if (grain[0] !== 500 || grain[1] !== 0) {
-      grains[grain[0] + "," + grain[1]] = true;
-    } else {
-      return Object.keys(grains).length + 1;
+    grains[grain[0] + "," + grain[1]] = true;
+
+    if (grain[0] === 500 && grain[1] === 0) {
+      return Object.keys(grains).length;
     }
   }
 }
