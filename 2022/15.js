f3b83dd07878e26a7cf9f4a77b9d748d556373ed
Marek Magdziak <marek.magdziak@hotmail.cz>
2022-12-15 07:21:11 +0100

    cleanup

--- 2022/15.js
+++ 2022/15.js
@@ -12,70 +12,65 @@ const {
   makeGraph,
 } = require("../utils");
 
-function manhattanDistance(a, b) {
-  return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
-}
-
-function getPointsInManhattanDistance(position, distance) {
-  const points = [];
-  for (let x = position.x - distance; x <= position.x + distance; x++) {
-    for (let y = position.y - distance; y <= position.y + distance; y++) {
-      if (manhattanDistance(position, { x, y }) <= distance) {
-        points.push({ x, y });
-      }
-    }
-  }
-  return points;
-}
-
 const input = fs
   .readFileSync("15.txt")
   .toString()
   .trim()
   .split("\n")
-  //Sensor at x=2, y=18: closest beacon is at x=-2, y=15
   .map((e) => ({
-    sensor: { x: parseInt(e.split(' ')[2].split('=')[1]), y: parseInt(e.split(' ')[3].split('=')[1]) },
-    beacon: { x: parseInt(e.split(' ')[8].split('=')[1]), y: parseInt(e.split(' ')[9].split('=')[1]) },
+    sensor: {
+      x: parseInt(e.split(" ")[2].split("=")[1]),
+      y: parseInt(e.split(" ")[3].split("=")[1]),
+    },
+    beacon: {
+      x: parseInt(e.split(" ")[8].split("=")[1]),
+      y: parseInt(e.split(" ")[9].split("=")[1]),
+    },
   }));
 
-function getNonExistingRange(row) {
-  const distances = input.map(e => ({
-    sensor: e.sensor,
-    beacon: e.beacon,
-    freeRadius: manhattanDistance(e.sensor, e.beacon)
-  }));
-  
-  let d = distances.map(e => ({
-    ydistance: Math.abs(e.sensor.y - row),
-    radius: Math.max(0, e.freeRadius - Math.abs(e.sensor.y - row)),
-    xBeacon: e.sensor.x,
-    xFrom: e.sensor.x - Math.abs(e.freeRadius - Math.abs(e.sensor.y - row)),
-    xTo: e.sensor.x + Math.abs(e.freeRadius - Math.abs(e.sensor.y - row)),
-  })).filter(e => e.radius > 0).sort((a, b) => a.xFrom - b.xFrom);
-
-  let ranges = [];
-  for (let g of d) {
-    let range = ranges.find(e => e.xFrom <= g.xFrom && e.xTo >= g.xFrom);
-    if (range) {
-      range.xTo = Math.max(range.xTo, g.xTo);
+function joinRanges(ranges) {
+  let result = [];
+  for (let range of ranges) {
+    let last = result[result.length - 1];
+    if (last && last.xTo + 1 >= range.xFrom) {
+      last.xTo = Math.max(last.xTo, range.xTo);
     } else {
-      ranges.push({ xFrom: g.xFrom, xTo: g.xTo } );
+      result.push(range);
     }
   }
+  return result;
+}
 
-  return ranges;
+function getNonExistingRange(row) {
+  const distances = input.map((e) => ({
+    x: e.sensor.x,
+    y: e.sensor.y,
+    radius:
+      Math.abs(e.sensor.x - e.beacon.x) + Math.abs(e.sensor.y - e.beacon.y),
+  }));
+
+  return joinRanges(
+    distances
+      .map((e) => ({
+        xFrom: e.x - (e.radius - Math.abs(e.y - row)),
+        xTo: e.x + (e.radius - Math.abs(e.y - row)),
+      }))
+      .filter((e) => e.xFrom <= e.xTo)
+      .sort((a, b) => a.xFrom - b.xFrom)
+  );
 }
 
 function part1() {
-  const ranges = getNonExistingRange(10);
-  return ranges.reduce((p, x) => p + (x.xTo - x.xFrom), 0);
+  return getNonExistingRange(2000000).reduce(
+    (p, x) => p + (x.xTo - x.xFrom),
+    0
+  );
 }
 
 function part2() {
   for (let row = 0; row < 4000000; row++) {
     const ranges = getNonExistingRange(row);
-    if (ranges.length > 1 && ranges[0].xTo !== ranges[1].xFrom) {
+    if (ranges.length > 1) {
       return (ranges[0].xTo + 1) * 4000000 + row;
     }
   }
